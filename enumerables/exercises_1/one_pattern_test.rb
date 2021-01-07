7a9ae98193c96524f9b900577c063becc96bd124
George Ho <george.h.w.ho@gmail.com>
2021-01-07 18:42:12 -0500

    completed enums one? test

--- enumerables/exercises_1/one_pattern_test.rb
+++ enumerables/exercises_1/one_pattern_test.rb
@@ -19,40 +19,56 @@ class OnePatternTest < Minitest::Test
     count = 0
     ornaments.each do |ornament|
       # Your code goes here
+      count += 1 if ornament == 'ring'
     end
     exactly_one_ring = count == 1
     assert_equal false, exactly_one_ring
   end
 
   def test_not_just_one_ring
-    skip
+    # skip
     ornaments = ["bracelet", "ring", "ring", "anklet", "earring"]
     count = 0
     # Your code goes here
+    ornaments.each do |o|
+      count += 1 if o == 'ring'
+    end
     exactly_one_ring = count == 1
     assert_equal false, exactly_one_ring
   end
 
   def test_one_time
-    skip
+    # skip
     words = ["morning", "time", "evening", "noon", "dusk", "dawn"]
     # Your code goes here
+    count = 0
+    words.each do |w|
+      count += 1 if w == 'time'
+    end
     exactly_one_time = count == 1
     assert_equal true, exactly_one_time
   end
 
   def test_one_double_digit_number
-    skip
+    # skip
     numbers = [8, 2, 10, 333, 9, 101]
     # Your code goes here
+    count = 0
+    numbers.each do |num|
+      count += 1 if num.to_s.size == 2
+    end
     exactly_one_double_digit = count == 1
     assert_equal true, exactly_one_double_digit
   end
 
   def test_not_one_even_number
-    skip
+    # skip
     numbers = [3, 20, 81, 10, 391, 32]
     # Your code goes here
+    count = 0
+    numbers.each do |num|
+      count += 1 if num % 2 == 2
+    end 
     exactly_one_even_number = count == 1
     assert_equal false, exactly_one_even_number
   end
