95134cbbd4b9a76edb557afbedc938d40b5ba49e
George Ho <george.h.w.ho@gmail.com>
2021-01-07 18:29:29 -0500

    completed enums none? test

--- enumerables/exercises_1/none_pattern_test.rb
+++ enumerables/exercises_1/none_pattern_test.rb
@@ -18,36 +18,52 @@ class NonePatternTest < Minitest::Test
     not_none_negative = true
     numbers.each do |number|
       # Your code goes here
+      not_none_negative = false if number != number.abs
     end
     assert_equal false, not_none_negative
   end
 
   def test_none_are_negative
-    skip
+    # skip
     numbers = [9, 3, 1, 8, 3, 3, 5]
     none_negative = true
     # Your code goes here
+    numbers.each do |num|
+      none_negative = false if num != num.abs
+    end
     assert_equal true, none_negative
   end
 
   def test_none_shall_pass
-    skip
+    # skip
     critters = ["elf", "hobbit", "dwarf", "wizard", "human"]
     # Your code goes here
+    none_shall_pass = true
+    critters.each do |crit|
+      none_shall_pass = false if crit == 'balrog'
+    end
     assert_equal true, none_shall_pass
   end
 
   def test_one_or_more_shall_pass
-    skip
+    # skip
     phrases = ["go", "left", "can cross", "shall pass", "must stay", "tarried"]
     # Your code goes here
+    none_shall_pass = true
+    phrases.each do |p|
+      none_shall_pass = false if p == 'shall pass'
+    end
     assert_equal false, none_shall_pass
   end
 
   def test_none_even
-    skip
+    # skip
     numbers = [3, 9, 15, 21, 19]
     # Your code goes here
+    none_even = true
+    numbers.each do |num|
+      none_even = false if num % 2 == 0
+    end 
     assert_equal true, none_even
   end
 
